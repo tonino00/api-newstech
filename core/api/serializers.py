0909e748fd24cef4e94d0d662804e73f67010173
tonino00 <antoninocgama@hotmail.com>
2018-05-31 01:04:06 -0300

    ajuste

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import Noticia
 
+
 class NoticiaSerializer(ModelSerializer):
     class Meta:
         model = Noticia
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mambo/code/api-newstech/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
